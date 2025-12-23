31b9391c57da5d3c4821c72c387bb4f2ef42fb95
Jakub Konieczny <jkonieczny@student.agh.edu.pl>
2025-12-23 18:48:46 +0100

    fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,13 +5,6 @@ from sentiment_app import app
 client = TestClient(app)
 
 
-def test_empty_string():
-    response = client.post("/predict", json={"text": ""})
-
-    assert response.status_code == 422
-    assert response.json() == {"detail": "Input text should be a non-empty string"}
-
-
 def test_valid_response():
     response = client.post("/predict", json={"text": "Test"})
     assert response.status_code == 200
